dbd5fc3b1868fc929a2229bf4065ffe363f0c3f9
Christina Gonzalez <cmgamway@gmail.com>
2019-08-27 08:09:30 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def my_each (words)
   if block_given?
   i= 0
@@ -12,5 +13,18 @@ def my_each (words)
     raise i
     my_each = "#{tas} #{i}"
   end
+=======
+def my_each (array)
+  i= 0
+
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
+
+  fruits = ["apple", "banana", "orange", "mango"]
+  my_each(fruits) do |i|
+    print i
+>>>>>>> 4a0834ee45ea6a07709e11d7e1d85a1e86aec8bf
 end
 end
